fa0017b8a2a0b7f66857de45a29bf5fb2f7e6bd8
wanatchaporn9339 <73693820+wanatchaporn9339@users.noreply.github.com>
2020-11-19 23:49:43 +0700

    Update show.php

--- show.php
+++ show.php
@@ -67,14 +67,14 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
       <td>
         <form method="POST" action="delete.php">
          <input type="hidden" name="id" value="<?php echo $Result['ID']; ?>" />
-        <button type="submit" class="btn btn-danger" >Delete</button>
+        <button type="submit" class="alert alert-danger" >Delete</button>
         </form>
         <form method="POST" action="edit.php">
          <input type="hidden" name="id" value="<?php echo $Result['ID']; ?>" />
           <input type="hidden" name="name" value="<?php echo $Result['Name']; ?>" />
           <input type="hidden" name="comment" value="<?php echo $Result['Comment']; ?>" />
           <input type="hidden" name="link" value="<?php echo $Result['Link']; ?>" />
-        <button type="submit" class="btn btn-secondary" >Edit</button>
+        <button type="submit" class="alert alert-dark" >Edit</button>
         </form>
       </td>
     </tr>

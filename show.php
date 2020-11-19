acb57ec424029794fa83f7f06d938960336acea1
wanatchaporn9339 <73693820+wanatchaporn9339@users.noreply.github.com>
2020-11-19 20:44:00 +0700

    Update show.php

--- show.php
+++ show.php
@@ -13,7 +13,7 @@
 
 <?php
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'database-of-hnunnz.mysql.database.azure.com', 'filmzz@databaselabitf', '#nun0993729339', 'itflab', 3306);
+mysqli_real_connect($conn, 'database-of-hnunnz.mysql.database.azure.com', 'wanatchaporn@database-of-hnunnz', '#nun0993729339', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());

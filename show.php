439cb6873a8febc7ecec1d244910e67234f9bdff
wanatchaporn9339 <73693820+wanatchaporn9339@users.noreply.github.com>
2020-11-20 13:09:51 +0700

    Update show.php

--- show.php
+++ show.php
@@ -22,7 +22,7 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
 <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div class="container">
-      <a class="navbar-brand" href="show.php">Start Bootstrap</a>
+      <a class="alert alert-info" href="show.php">Lab Database</a>
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         Menu
         <i class="fas fa-bars"></i>
